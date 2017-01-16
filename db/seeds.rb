f5857f644ef07d04f4f4965c4b7108a64c352c66
Arthur Littmann <arthur.littmann@gmail.com>
2017-01-16 14:10:22 +0000

    added navbar with categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+look_book = Category.create(name: "Fashion")
+travel = Category.create(name: "Travel")
+art = Category.create(name: "Art")
+fun = Category.create(name: "Fun")
+inspiration = Category.create(name: "Inspiration")
