00d97e0fe656c6fad8f7b277360dc5459e9d0f88
Arthur Littmann <arthur.littmann@gmail.com>
2017-01-20 10:12:45 +0000

    trying to add error pages

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,12 @@ Bundler.require(*Rails.groups)
 
 module ElephantAndCastle
   class Application < Rails::Application
+
     config.generators do |generate|
       generate.assets false
     end
+
+    config.exceptions_app = self.routes
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
