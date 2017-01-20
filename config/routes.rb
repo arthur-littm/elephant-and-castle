bb37b660e3d514f18c427890510488c69116d531
Arthur Littmann <arthur.littmann@gmail.com>
2017-01-20 10:29:02 +0000

    404 with links

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,4 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Attachinary::Engine => "/attachinary"
-
-   [404, 422, 500].each do |code|
-    get code, :to => "errors#show", :code => code
-  end
 end
