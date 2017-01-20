49ebb94d48a6afca1b0870511bff91bab00763be
Arthur Littmann <arthur.littmann@gmail.com>
2017-01-20 11:00:43 +0000

    404 fix attempt

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,6 @@ module ElephantAndCastle
       generate.assets false
     end
 
-    config.exceptions_app = self.routes
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
