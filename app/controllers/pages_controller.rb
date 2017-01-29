39933b057bfb5b16b2fd194f12c05100c3489711
Arthur Littmann <arthur.littmann@gmail.com>
2017-01-29 21:52:36 +0000

    fixed searchbar and removed devise pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,21 @@
 class PagesController < ApplicationController
   def home
-    @posts = Post.all.order(created_at: :asc).first(10)
-    @videos = Video.all.order(created_at: :asc).first(10)
+    @posts = Post.all.order(created_at: :desc).first(4)
+    @videos = Video.all.order(created_at: :desc).first(4)
     @banner_post = Post.all.order(created_at: :asc).first
   end
 
   def search
     if params[:category]
 
-      @posts = Post.all.where(category: params[:category])
+      @posts = Post.all.where(category: params[:category]).order(created_at: :desc)
 
-      @videos = Video.all.where(category: params[:category])
+      @videos = Video.all.where(category: params[:category]).order(created_at: :desc)
 
 
     else
       @posts = Post.search(params[:search])
-      # Post.reindex
-
       @videos = Video.search(params[:search])
-      # Video.reindex
     end
 
   end
