0d0c092c8e49ab1dac9fcb5ca8620f6f41abbcb1
Arthur Littmann <arthur.littmann@gmail.com>
2017-02-14 10:51:12 +0000

    image uploader fixed

--- config/initializers/carrierwave.rb
+++
@@ -1,7 +0,0 @@
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     provider:              'day31wllx',                     # required
-#     aws_access_key_id:     '493233291649232',               # required
-#     aws_secret_access_key: 'AT7yx-3D7l5buBrr5nAnaHPlLuo',   # required
-#   }
-# end
